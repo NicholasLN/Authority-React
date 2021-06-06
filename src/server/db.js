97f5ab3b3a9ffedd8ff969126b89acdfc03a6ecb
PhilScott4VT <PhilScott4USA@gmail.com>
2021-06-06 10:51:24 -0400

    Error handling

--- src/server/db.js
+++ src/server/db.js
@@ -1,15 +1,26 @@
 var mysql = require('mysql');
-
-var server_configuration = requre('databaseConfig.json');
+const logger = require('node-color-log');
+try{
+    var server_configuration = require('./databaseConfig.json');
+}
+catch(e){
+    logger.color('red').bold().log("[mySQL] databaseConfig.json does not exist! Use databaseConfigExample.js as a template!");
+    return;
+}
 var con = mysql.createConnection(server_configuration);
 
+
 con.connect(function(err) {
-  if (err) throw err;
+    if(err){
+        if (err.code === "ER_DBACCESS_DENIED_ERROR"){
+            logger.color('red').bold().log("[mySQL] Incorrect DB Credentials! Check DB config details.");
+        }
+    }
 });
 
 con.on('error', err => {
     if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-        console.log("SQL Timeout")
+        logger.color('yellow').bold().log("[mySQL] SQL Timeout");
     } else {
         throw err;
     }
