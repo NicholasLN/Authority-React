cde39ca3044d7f7b861db7b4ee866307eb32f831
PhilScott4VT <PhilScott4USA@gmail.com>
2021-06-06 10:51:35 -0400

    removed ya mom

--- src/server/index.js
+++ src/server/index.js
@@ -3,6 +3,4 @@ let cookie_parser = require('cookie-parser');
 
 const app = express();
 
-//app.use(express.static('dist'));
-
 app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
