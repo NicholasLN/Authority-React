91e182815fcdf455db3062b90d59cdfd3413c792
PhilScott4VT <PhilScott4USA@gmail.com>
2021-06-10 17:08:33 -0400

    fix sessionInit.js

--- src/server/sessionInit.js
+++ src/server/sessionInit.js
@@ -2,11 +2,18 @@ const cookieSession = require('cookie-session');
 const logger = require("node-color-log");
 const dotenv = require('dotenv').config();
 
+if(process.env.SESSION_SECURE == "false"){
+    var secureSession = false;
+}
+else{
+    var secureSession = true;
+}
+
 try{
     var configDetails = {
         name:'authSession',
         keys:[process.env.COOKIE_SECRET],
-        secure:process.env.SESSION_SECURE,
+        secure:secureSession,
         maxAge: 24 * 60 * 60 * 1000,
     }
 }
