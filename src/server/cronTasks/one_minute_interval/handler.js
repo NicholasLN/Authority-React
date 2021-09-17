4d0647f59675e5ae0d929306d821b5d6121f15b2
PhilScott4VT <PhilScott4USA@gmail.com>
2021-09-17 16:03:18 -0400

    eeee

--- src/server/cronTasks/one_minute_interval/handler.js
+++ src/server/cronTasks/one_minute_interval/handler.js
@@ -2,7 +2,6 @@ const cron = require("node-cron");
 
 // At every minute
 cron.schedule("* * * * *", async () => {
-  console.log("sex");
   // Party vote check
   require("./partyVoteCheck.js");
 });
