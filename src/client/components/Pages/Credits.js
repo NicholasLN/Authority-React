487c0c756fb6d74bc0dc9707707235cb2c3e0aca
Phil Scott <PhilScott4USA@gmail.com>
2021-10-02 21:54:05 -0400

    credit style

--- src/client/components/Pages/Credits.js
+++ src/client/components/Pages/Credits.js
@@ -21,7 +21,7 @@ export default function Credits() {
     <Body middleColWidth="11">
       <h1>Credits and Thanks</h1>
       <div className="row">
-        <div className="col-md-6">
+        <div className="col-md-4">
           <h4>Contribution Credits:</h4>
           <hr />
           <b className="bold">Main Developer:</b> Phil Scott
@@ -30,13 +30,15 @@ export default function Credits() {
           <br />
           <b className="bold">Demographic Research:</b> Rainbow (Skyler Mizuki)
         </div>
-        <div className="col-md-6">
+        <div className="col-md-8">
           <h4>Patrons</h4>
           <hr />
           <p>A very special thanks to this group of contributors for helping with the expenses of this project.</p>
           <div className="row">
             <div className="col-md-4">
-              <b className="bold">AUTHORITY Bronze Patrons:</b>
+              <b className="bold" style={{ color: "#CD7F32" }}>
+                AUTHORITY Bronze Patrons:
+              </b>
               {Object.keys(patrons).map((patron) => {
                 let patr = patrons[patron];
                 if (patr.rank == "bronze") {
@@ -51,7 +53,9 @@ export default function Credits() {
               <br />
             </div>
             <div className="col-md-4">
-              <b className="bold">AUTHORITY Silver Patrons:</b>
+              <b className="bold" style={{ color: "silver" }}>
+                AUTHORITY Silver Patrons:
+              </b>
               {Object.keys(patrons).map((patron) => {
                 let patr = patrons[patron];
                 if (patr.rank == "silver") {
@@ -66,7 +70,9 @@ export default function Credits() {
               <br />
             </div>
             <div className="col-md-4">
-              <b className="bold">AUTHORITY Gold Patrons:</b>
+              <b className="bold" style={{ color: "#D4AF37" }}>
+                AUTHORITY Gold Patrons:
+              </b>
               {Object.keys(patrons).map((patron) => {
                 let patr = patrons[patron];
                 if (patr.rank == "gold") {
