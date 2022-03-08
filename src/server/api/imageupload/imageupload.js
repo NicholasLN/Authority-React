41e5fde08e9af2a26c40db8f1fa10be01c19f04d
NicholasLN <PhilScott4USA@gmail.com>
2022-03-08 10:02:16 -0500

    upload enabled

--- src/server/api/imageupload/imageupload.js
+++ src/server/api/imageupload/imageupload.js
@@ -22,44 +22,42 @@ router.post("/postCustomPicture", async function (req, res) {
 });
 
 router.post("/newUserImg", async function (req, res) {
-  // if (req.session.playerData.loggedIn) {
-  //   var mb = (req.file.size / (1024 * 1024)).toFixed(2);
-  //   if (mb <= process.env.MAX_UPLOAD_SIZE) {
-  //     var url = await uploadImage(req.file, `users/${req.session.playerData.loggedInId}`).catch((err) => {
-  //       res.send({ error: err });
-  //     });
-  //     res.send({ url });
-  //   } else {
-  //     res.send({ error: "Image too large." });
-  //   }
-  // } else {
-  //   res.send({ error: "Not logged in." });
-  // }
-  res.send({error:"Image upload disabled for now."});
+  if (req.session.playerData.loggedIn) {
+    var mb = (req.file.size / (1024 * 1024)).toFixed(2);
+    if (mb <= process.env.MAX_UPLOAD_SIZE) {
+      var url = await uploadImage(req.file, `users/${req.session.playerData.loggedInId}`).catch((err) => {
+        res.send({ error: err });
+      });
+      res.send({ url });
+    } else {
+      res.send({ error: "Image too large." });
+    }
+  } else {
+    res.send({ error: "Not logged in." });
+  }
 });
 
 router.post("/newPartyImg", async function (req, res) {
-  // if (req.session.playerData.loggedIn) {
-  //   var party = new Party(req.session.playerData.loggedInInfo.party);
-  //   await party.updatePartyInfo();
-  //   if (userHasPerm(req.session.playerData.loggedInId, party.partyInfo, "leader")) {
-  //     var mb = (req.file.size / (1024 * 1024)).toFixed(2);
-  //     if (mb <= process.env.MAX_UPLOAD_SIZE) {
-  //       var url = await uploadImage(req.file, `parties/${party.partyID}`).catch((err) => {
-  //         res.send({ error: err });
-  //       });
-  //       var partyInfo = await party.updateParty("partyPic", url);
-  //       res.send(partyInfo);
-  //     } else {
-  //       res.send({ error: "Image too large." });
-  //     }
-  //   } else {
-  //     res.send({ error: "Not leader of party." });
-  //   }
-  // } else {
-  //   res.send({ error: "Not logged in." });
-  // }
-  res.send({error:"Image upload disabled for now."})
+  if (req.session.playerData.loggedIn) {
+    var party = new Party(req.session.playerData.loggedInInfo.party);
+    await party.updatePartyInfo();
+    if (userHasPerm(req.session.playerData.loggedInId, party.partyInfo, "leader")) {
+      var mb = (req.file.size / (1024 * 1024)).toFixed(2);
+      if (mb <= process.env.MAX_UPLOAD_SIZE) {
+        var url = await uploadImage(req.file, `parties/${party.partyID}`).catch((err) => {
+          res.send({ error: err });
+        });
+        var partyInfo = await party.updateParty("partyPic", url);
+        res.send(partyInfo);
+      } else {
+        res.send({ error: "Image too large." });
+      }
+    } else {
+      res.send({ error: "Not leader of party." });
+    }
+  } else {
+    res.send({ error: "Not logged in." });
+  }
 });
 
 module.exports.router = router;
